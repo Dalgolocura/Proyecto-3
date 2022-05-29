8a8a0888174e5ff24bd25148b1eaae2e7eb7b296
WilliamMendez <69637352+WilliamMendez@users.noreply.github.com>
2022-05-28 19:53:23 -0500

    entrada estandar

--- ProblemaP3.py
+++ ProblemaP3.py
@@ -1,28 +1,47 @@
-entrada= "JulianaJulinulinuliuii"
-salida = ""
+from sys import stdin
+from time import process_time as timer
 
-letras_repeticiones={}
 
-for n in  range(1,len(entrada)+1,1):
-    actual = entrada[-n]
-    if actual in letras_repeticiones:
-        letras_repeticiones[actual]+=1
-    else:
-        letras_repeticiones[actual]=1
+def lectura():
+    start = timer()
+    nCasos = int(stdin.readline())
+    # print(nCasos)
+    while nCasos != 0:
+        linea = stdin.readline().replace('\n', '')
+        # print("lista", lista)
+        procesar(linea)
+        nCasos -= 1
 
-letras= list(letras_repeticiones.keys())
+    elapsed_time = timer() - start
+    print("Time: %.10f" % elapsed_time)
 
-letras_cantidad={}
-orden_eliminacion= ""
 
-for n in range(1,len(letras)+1,1):
-    letra = letras[-n]
-    veces= letras_repeticiones[letra]//n
-    letras_cantidad[letra]=veces
-    orden_eliminacion += letra
+def procesar(entrada):
+    letras_repeticiones = {}
 
-cantidad_palabra = sum(list(letras_cantidad.values()))
-palabra_original = entrada[0:cantidad_palabra]
+    for n in range(1, len(entrada)+1, 1):
+        actual = entrada[-n]
+        if actual in letras_repeticiones:
+            letras_repeticiones[actual] += 1
+        else:
+            letras_repeticiones[actual] = 1
 
-salida = palabra_original + " " +orden_eliminacion
-print(salida)
+    letras = list(letras_repeticiones.keys())
+
+    letras_cantidad = {}
+    orden_eliminacion = ""
+
+    for n in range(1, len(letras)+1, 1):
+        letra = letras[-n]
+        veces = letras_repeticiones[letra]//n
+        letras_cantidad[letra] = veces
+        orden_eliminacion += " " + letra
+
+    cantidad_palabra = sum(list(letras_cantidad.values()))
+    palabra_original = entrada[0:cantidad_palabra]
+
+    salida = palabra_original + orden_eliminacion
+    print(salida)
+
+
+lectura()
