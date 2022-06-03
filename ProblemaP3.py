06910a2224e07d1b7adf434b89343712eca8938b
Daniel Aguilera <d.aguilera@uniandes.edu.co>
2022-06-03 14:16:41 -0500

    yastubo

--- ProblemaP3.py
+++ ProblemaP3.py
@@ -2,6 +2,13 @@ from sys import stdin
 from time import process_time as timer
 # import threading
 
+""" 
+William Mendez 202012662
+Juliana Galeano 202012128
+Daniel Aguilera 202010592
+Boris N. Reyes R. 202014743
+"""
+
 
 def lectura():
     start = timer()
@@ -11,20 +18,6 @@ def lectura():
         procesar(linea)
         nCasos -= 1
 
-    elapsed_time = timer() - start
-    print("Time: %.10f" % elapsed_time)
-
-
-def lecturaConArchivo():
-    start = timer()
-    file = open("jijijija.in", "r")
-    # file = open("500000.in", "r")
-    nCasos = int(file.readline())
-    while nCasos != 0:
-        linea = file.readline().replace('\n', '')
-        procesar(linea)
-        # procesarCifrando(linea)
-        nCasos -= 1
     elapsed_time = timer() - start
     print("Time: %.10f" % elapsed_time)
 
@@ -71,36 +64,6 @@ def procesar(entrada):
         print("NO EXISTE")
 
 
-def procesarCifrando(entrada):
-
-    letras_repeticiones = {}
-    orden = []
-
-    for n in range(1, len(entrada)+1):
-        actual = entrada[-n]
-        if letras_repeticiones.get(actual) is not None:
-            letras_repeticiones[actual] += 1
-        else:
-            letras_repeticiones[actual] = 1
-            orden.append(actual)
-
-    # print(orden)
-
-    for i in range(len(orden), len(entrada)):
-        palabra = entrada[:i]
-
-        # print("palabra", palabra)
-
-        cifrada = cifrarCadena(palabra, orden)
-        # print("cifrada", cifrada)
-        if cifrada == entrada:
-            print(cifrada, ''.join(orden[::-1]))
-            break
-        elif i == len(entrada)-1:
-            print("NO EXISTE")
-
-
-
 def cifrarCadena(cadena: str, letras: list):
     cadenaCopy = cadena
     index = 0
@@ -113,6 +76,5 @@ def cifrarCadena(cadena: str, letras: list):
         cadena += cadenaCopy
     return cadena
 
-lectura()
-# lecturaConArchivo()
 
+lectura()
